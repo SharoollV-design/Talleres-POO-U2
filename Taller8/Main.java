89b0853c84daa37c6b4475cc64022726f9237c0d
Sharooll <smezav1@unicartagena.edu.co>
2026-05-19 08:51:42 -0500

    Ejercicio 1: Cree una clase main para comprobar el funcionamiento de las clases

---
+++ Taller8/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    
+    public static void main(String[] args) {
+
+        Coche coche1 = new Coche();
+
+        coche1.setMarca("Mazda");
+        coche1.setVelocidadMarca(300);
+        coche1.setNumeroDePuertas(4);
+        coche1.mostrarInfo();
+
+        System.out.println(" ");
+    }
+}
