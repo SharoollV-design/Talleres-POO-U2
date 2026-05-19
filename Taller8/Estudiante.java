47a114a202bb62ac05f7ef74e397ad7a6e7df4f1
Sharooll <smezav1@unicartagena.edu.co>
2026-05-19 09:28:01 -0500

    Ejercicio 2: Cree una clase hija llamada estudiante que hereda algunos atributos de su clase padre

---
+++ Taller8/Estudiante.java
@@ -0,0 +1,32 @@
+public class Estudiante extends Persona {
+    
+    private String matricula;
+
+    public Estudiante(){
+
+    }
+
+    public Estudiante(String nombre, int edad, String matricula) {
+        super(nombre, edad);
+        this.matricula = matricula;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        if (matricula != null && !matricula.isEmpty()) {
+            this.matricula = matricula;
+        } else {
+            System.out.println("La matricula no puede estar vacia");
+        }
+    }
+
+    @Override
+    public void mostrarInfo(){
+
+        super.mostrarInfo();
+        System.out.println("Matricula: " + matricula);
+    }
+}
