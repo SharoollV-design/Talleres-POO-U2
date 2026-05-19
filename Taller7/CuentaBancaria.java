f5512be3331a8116b5291a28aac7eb7b18e30353
Sharooll <smezav1@unicartagena.edu.co>
2026-05-18 21:10:13 -0500

    Ejercicio 2, Clase Cuenta Bancaria con metodos set y get

---
+++ Taller7/CuentaBancaria.java
@@ -0,0 +1,36 @@
+public class CuentaBancaria {
+    
+    private String numeroCuenta;
+    private double saldo;
+    public String tipoCuenta;
+
+    public CuentaBancaria(String numeroCuenta, double saldo, String tipoCuenta) {
+        this.numeroCuenta = numeroCuenta;
+        this.tipoCuenta = tipoCuenta;
+        setSaldo(saldo);
+    }
+
+    public double getSaldo(){
+        return saldo;
+    }
+
+    public void setSaldo(double saldo){
+        if (saldo < 0) {
+            System.out.println("El saldo no puede ser negativo");
+            return;
+        }
+
+        this.saldo = saldo;
+        System.out.println("Saldo actualizado");
+    }
+
+    public void mostrarinfo(){
+        
+        System.out.println("== Cuentas bancarias Bancolombia ==");
+        System.out.println("Numero de cuenta: " + numeroCuenta);
+        System.out.println("Tipo de cuenta: " + tipoCuenta);
+        System.out.println("Saldo disponible: " + saldo);
+        System.out.println(" ==== ====");
+    }
+
+}
