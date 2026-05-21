9f70db12ef0e76af6e2e829869acd442c15c44bf
Sharooll <smezav1@unicartagena.edu.co>
2026-05-20 23:10:00 -0500

    Ejercicio 1: Cree una clase persona con un método publico de presentarse

---
+++ Taller10/Persona.java
@@ -0,0 +1,12 @@
+public class Persona {
+    
+    protected String nombre;
+
+    public Persona(String nombre){
+        this.nombre = nombre;
+    }
+
+    public void presentarse(){
+        System.out.println(" Holaaa soy una persona ");
+    }
+}
