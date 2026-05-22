e393669199e818e66ed8a59bb535f7b3247d77ac
Sharooll <smezav1@unicartagena.edu.co>
2026-05-21 22:50:39 -0500

    Ejercicio 2: Cree una clase de prueba para comprobar el funcionamiento

---
+++ Taller10/PruebaVehiculo.java
@@ -0,0 +1,11 @@
+public class PruebaVehiculo {
+    
+    public static void main(String[] args) {
+        
+        Vehiculo v1 = new Vehiculo();
+        Vehiculo v2 = new Bicicleta();
+
+        v1.moverse();
+        v2.moverse();
+    }
+}
