7d8048ca2de44928766c3c369d6e872e50d3b726
Sharooll <smezav1@unicartagena.edu.co>
2026-05-21 22:55:22 -0500

    Ejercicio 3: Visualice los posibles errores al utilizar el @Override

--- Taller10/Bicicleta.java
+++ Taller10/Bicicleta.java
@@ -1,6 +1,8 @@
 public class Bicicleta extends Vehiculo {
     
     @Override
+    // public void moverse(String pedalear) {
+    // Es un error porque no está sobrescribiendo, esta llamando un metodo nuevo
     public void moverse(){
         System.out.println("La bicicleta está en movimiento");
     }
