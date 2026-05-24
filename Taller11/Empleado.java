f7148af788a6c8379fa82588e57c5abc5e7a7f37
Sharooll <smezav1@unicartagena.edu.co>
2026-05-24 14:53:28 -0500

    Ejercicio 2: Cree una clase abstracta padre, con atributos y constructores, además agregue un método abstracto

---
+++ Taller11/Empleado.java
@@ -0,0 +1,27 @@
+abstract  class Empleado {
+
+    private String nombre;
+    
+    public Empleado(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        if(nombre != null && !nombre.trim().isEmpty()) {
+            this.nombre = nombre;
+        } else {
+            System.out.println("El espacio no debe estar vacío");
+        }
+    }
+    
+    abstract double calcularSalario();
+
+    void mostrarSalario(){
+        System.out.println("Nombre del empleado: " + getNombre());
+        System.out.println("El salario actual es:");
+    }
+}
