022aeee0cd2e07071415e754ae0161957470210b
Sharooll <smezav1@unicartagena.edu.co>
2026-05-24 14:30:50 -0500

    Ejercicio 1: Hice una clase de prueba de Figura en donde comprobé el funcionamiento de las clases y la herencia

---
+++ Taller11/PruebaFigura.java
@@ -0,0 +1,11 @@
+public class PruebaFigura {
+    
+    public static void main(String[] args) {
+        
+        Figura circulo = new Circulo(5);
+        Figura rectangulo = new Rectangulo(7, 3);
+
+        circulo.mostrarArear();
+        rectangulo.mostrarArear();
+    }
+}
