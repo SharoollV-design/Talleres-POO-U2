84fabdaf5c94a1c4a35c5dd9c1ee453242b994be
Sharooll <smezav1@unicartagena.edu.co>
2026-05-24 21:27:24 -0500

    Ejercicio 1: Cree una clase de prueba para ave, utilizando polimorfismo desde las interfaces

---
+++ Taller12/PruebaAve.java
@@ -0,0 +1,15 @@
+public class PruebaAve {
+    
+    public static void main(String[] args) {
+        
+        Volador ave1 = new Ave("Kimmy");
+
+        Cantante ave2 = new Ave("Wilson");
+
+        ave1.volar();
+
+        System.out.println(" ");
+
+        ave2.cantar();
+    }
+}
